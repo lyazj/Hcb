18370b364b7247cabc575dfe4877f2a4eb71c581
lyazj <seeson@pku.edu.cn>
2025-03-10 10:07:28 +0100

    Good luck!

---
+++ org/download.sh
@@ -0,0 +1,8 @@
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL18wmLHEGEN-08760 -O 1-HIG-RunIISummer20UL18wmLHEGEN-08760.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL18SIM-08147 -O 2-HIG-RunIISummer20UL18SIM-08147.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL18DIGIPremix-08128 -O 3-HIG-RunIISummer20UL18DIGIPremix-08128.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL18HLT-08147 -O 4-HIG-RunIISummer20UL18HLT-08147.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL18RECO-08147 -O 5-HIG-RunIISummer20UL18RECO-08147.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL18MiniAODv2-08147 -O 6-HIG-RunIISummer20UL18MiniAODv2-08147.sh &
+wget https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-RunIISummer20UL18NanoAODv9-07464 -O 7-HIG-RunIISummer20UL18NanoAODv9-07464.sh &
+wait
